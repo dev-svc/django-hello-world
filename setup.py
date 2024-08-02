816831400ae5960bf03024e3aef0bb27393e6719
sunandad2090 <149049323+sunandad2090@users.noreply.github.com>
2024-08-02 14:22:49 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-setup {
-   name="django-hello-world"
-   version="1.0.0"
-   description="Django Python App"
-}
+setup(
+   name="django-hello-world",
+   version="1.0.0",
+   description="Django Python App",
+)
